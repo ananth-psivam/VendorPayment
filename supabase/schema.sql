381cc313ae9ccceca454a3c1f84b668cf428b6b0
ananth-psivam <ananth.p@prodapt.com>
2025-10-24 15:44:06 -0500

    Update schema.sql

--- supabase/schema.sql
+++ supabase/schema.sql
@@ -1,10 +1,10 @@
 create table if not exists public.invoices (
   Supplier_Name text primary key,
   Invoice_Date text,
-  Total_Invoice Amount text,
-  Currency_text default 'USD',,
-  status_text check (status in ('Paid','Unpaid','Queued','On Hold','Rejected','Not Found')),
-  Supplier_Invoice_No. text,
-  Comments_text,
-  Supplier_Invoice Date date,
+  Total_Invoice_Amount text,
+  Currency text default 'USD',,
+  Status text check (status in ('Paid','Unpaid','Queued','On Hold','Rejected','Not Found')),
+  Supplier_Invoice_No text,
+  Comments text,
+  Supplier_Invoice_Date date,
   );
